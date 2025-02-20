3f6f7e5f3678948c9e7449455eb59032ee3820f1
hunter-tx <hughesh97@outlook.com>
2025-02-20 10:58:39 -0600

    Expanded graphical drawing abilities to draw a line between 2 points given a color and the points

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ from window import *
 
 def main():
     win = Window(800, 600)
+    point1 = Point(100, 200)
+    point2 = Point(200, 500)
+    point3 = Point(600, 300)
+    point4 = Point(500, 500)
+    line1 = Line(point1, point2)
+    line2 = Line(point2, point3)
+    line3 = Line(point3, point4)
+    line4 = Line(point4, point1)
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "red")
+    win.draw_line(line3, "blue")
+    win.draw_line(line4, "black")
     win.wait_for_close()
 
 main()
--- window.py
+++ window.py
@@ -1,24 +1,27 @@
 from tkinter import Tk, BOTH, Canvas
+from point import *
 
 class Window:
     def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title("Hunter's Maze Solver")
-        self.canvas = Canvas(self.root, bg="white", width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root = Tk()
+        self.__root.title("Hunter's Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
         print("window closed...")
 
     def close(self):
-        self.running = False
+        self.__running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
