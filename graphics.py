a6eb7e8ddfcb1bcafed527c734bab069cbdd70a1
hunter-tx <hughesh97@outlook.com>
2025-02-20 14:54:10 -0600

    Created Cell class used to create a box which we can decide which sides are drawn

--- graphics.py
+++ graphics.py
@@ -1,29 +1,30 @@
+from operator import truediv
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Hunter's Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
-        self.__canvas.pack(fill=BOTH, expand=1)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.root = Tk()
+        self.root.title("Hunter's Maze Solver")
+        self.canvas = Canvas(self.root, bg="white", width=width, height=height)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
+        self.root.update_idletasks()
+        self.root.update()
 
     def wait_for_close(self):
-        self.__running = True
-        while self.__running:
+        self.running = True
+        while self.running:
             self.redraw()
         print("window closed...")
 
     def close(self):
-        self.__running = False
+        self.running = False
 
-    def draw_line(self, line, fill_color):
-        line.draw(self.__canvas, fill_color)
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
 
 
 class Point:
@@ -36,7 +37,7 @@ class Line:
         self.start = start
         self.end = end
 
-    def draw(self, canvas, fill_color):
+    def draw(self, canvas, fill_color="black"):
         canvas.create_line(
             self.start.x,
             self.start.y,
@@ -45,3 +46,34 @@ class Line:
             fill=fill_color,
             width=2
         )
+
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
--- main.py
+++ main.py
@@ -2,18 +2,22 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
-    point1 = Point(100, 200)
-    point2 = Point(200, 500)
-    point3 = Point(600, 300)
-    point4 = Point(500, 500)
-    line1 = Line(point1, point2)
-    line2 = Line(point2, point3)
-    line3 = Line(point3, point4)
-    line4 = Line(point4, point1)
-    win.draw_line(line1, "black")
-    win.draw_line(line2, "red")
-    win.draw_line(line3, "blue")
-    win.draw_line(line4, "black")
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 main()
