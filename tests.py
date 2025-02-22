3024e5b765dc39e64864e6b5b827d17e75a1897d
hunter-tx <hughesh97@outlook.com>
2025-02-22 09:02:22 -0600

    created method in the maze class to initiate creation at the entrance and exit of the maze

--- tests.py
+++ tests.py
@@ -42,6 +42,23 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_break_entrance_and_exit1(self):
+        num_cols = 16
+        num_rows = 16
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1._break_entrance_and_exit()
+        test = (m1._cells[0][0].has_top_wall, m1._cells[-1][-1].has_bottom_wall)
+        expected = (False, False)
+        self.assertEqual(test, expected)
+
+    def test_break_entrance_and_exit2(self):
+        num_cols = 25
+        num_rows = 1
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        m1._break_entrance_and_exit()
+        test = (m1._cells[0][0].has_top_wall, m1._cells[-1][-1].has_bottom_wall)
+        expected = (False, False)
+        self.assertEqual(test, expected)
 
 if __name__ == "__main__":
     unittest.main()
